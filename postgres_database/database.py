01fa32380ffc5a03cec848f5c5079086a1bf96da
Virgile-R <78867573+Virgile-R@users.noreply.github.com>
2022-02-14 11:05:02 +0100

    Update database.py

--- postgres_database/database.py
+++ postgres_database/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # defines a SQLDatabase Adress (defined in environnement variable for dev)
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 
 # creates an SQLAlchemy engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
